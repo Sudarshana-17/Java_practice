9ca4e175724a1a1cc6e408f55e907ca9547526a9
Sudarshana-17 <sudarshanw7353@gmail.com>
2024-04-02 11:27:17 +0530

    'l'

---
+++ fullstack/Backend/Languages/Java/Corejava/Core_java/src/data_types/CustomDataType.java
@@ -0,0 +1,26 @@
+package data_types;
+
+public class CustomDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=2;
+		int num2=2;
+		System.out.println(num1+num2);
+		
+		String greeting="goodmorning";
+				System.out.println(greeting);
+				char letter='a';
+				System.out.println(letter);
+				boolean jump=true;
+				System.out.println(jump);
+				float num=1234.0f;
+				System.out.println(num);
+				double numm=12345678900d;
+				System.out.println(numm);
+				Integer ab=1234567;
+				System.out.println(ab);
+
+	}
+
+}
---
+++ fullstack/Backend/Languages/Java/Corejava/Core_java/src/data_types/DefaultArray.java
@@ -0,0 +1,18 @@
+package data_types;
+
+public class DefaultArray {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=30;
+		int[] all= {10,20,30};
+		System.out.println(a);
+		System.out.println(all[0]);
+		System.out.println(all[1]);
+		System.out.println(all[2]);
+		 for (int i = 0; i < all.length; i++) {
+	            System.out.println(all[i]);
+	        }
+		}
+
+}
